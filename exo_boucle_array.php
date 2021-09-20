c28b3d18eed8dd41b83ac34e55943d685590fe54
Julie De Deken <julie.ddkn@gmail.com>
2021-09-20 16:13:07 +0200

    Fin d'journée

--- exo_boucle_array.php
+++ exo_boucle_array.php
@@ -13,8 +13,8 @@
     //     echo "\n$i";
     // };
 
-    $vals = array(20, 50, 7, 54);
-    array_push($vals, 150, 300);
+    // $vals = array(20, 50, 7, 54);
+    // array_push($vals, 150, 300);
     // foreach ($vals as $val) {
     //     echo "$val\n";
     // };
@@ -22,11 +22,40 @@
     // echo "$vals[$i]\n";
     // }
 
-    function ShowArray(array $array){
-        foreach ($array as $value) {
-            echo "$value\n";
-        }
-    };
-    ShowArray($vals);
+    // function ShowArray(array $array){
+    //     foreach ($array as $value) {
+    //         echo "$value\n";
+    //     }
+    // };
+    // ShowArray($vals);
+
+    // function ReversedArray (array $array) {
+    //     $reverse = array_reverse($array);
+    //     print_r($reverse);
+    // };
+    // $myArray = array ("Julie", "Logan", "Doom");
+    // ReversedArray($myArray);
+
+    // $movie1 = array("Title" => "Ghost World", "Director" => "Terry Zwigoff", "Genre" => "Comedy, Drama, Independant");
+    // $movie2 = array("Title" => "Mysterious Skin", "Director" => "Gregg Araki", "Genre" => "Drama, Independant");
+    // $movie3 = array("Title" => "Jawbreaker", "Director" => "Darren Stein", "Genre" => "Comedy");
+    
+    // $movie1 += ["Release Date" => "2001"];
+    // $movie2 += ["Release Date" => "2004"];
+    // $movie3 += ["Release Date" => "1999"];
+
+    // function ShowMovie (array $movie) {
+    //     foreach ($movie as $name => $info) {
+    //         echo "$name - $info\n";
+    //     };
+    // };
+
+    // ShowMovie($movie1);
+    // ShowMovie($movie2);
+    // ShowMovie($movie3);
+
+    $actors = array ("James Duval","Fairuza Balk", "Joseph Gordon-Levitt", "Anjelica Huston", "Dev Patel");
+    
+
 
 ?>
