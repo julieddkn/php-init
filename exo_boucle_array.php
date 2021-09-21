839f6b5032233003cbe8e2970ee1fff7bda604db
Julie De Deken <julie.ddkn@gmail.com>
2021-09-21 13:55:09 +0200

    Fin d'journée bis

--- exo_boucle_array.php
+++ exo_boucle_array.php
@@ -13,6 +13,10 @@
     //     echo "\n$i";
     // };
 
+    // for ($i=10; $i <= 50 ; $i++) { 
+    //     echo "\n$i";
+    // };
+
     // $vals = array(20, 50, 7, 54);
     // array_push($vals, 150, 300);
     // foreach ($vals as $val) {
@@ -22,19 +26,20 @@
     // echo "$vals[$i]\n";
     // }
 
-    // function ShowArray(array $array){
+    // function showArray(array $array) : void {
     //     foreach ($array as $value) {
     //         echo "$value\n";
     //     }
     // };
-    // ShowArray($vals);
+    // showArray($vals);
 
-    // function ReversedArray (array $array) {
+    // function ReversedArray (array $array) : array {
     //     $reverse = array_reverse($array);
-    //     print_r($reverse);
+    //     return $reverse;
     // };
     // $myArray = array ("Julie", "Logan", "Doom");
-    // ReversedArray($myArray);
+    // print_r(ReversedArray($myArray));
+    
 
     // $movie1 = array("Title" => "Ghost World", "Director" => "Terry Zwigoff", "Genre" => "Comedy, Drama, Independant");
     // $movie2 = array("Title" => "Mysterious Skin", "Director" => "Gregg Araki", "Genre" => "Drama, Independant");
@@ -54,8 +59,43 @@
     // ShowMovie($movie2);
     // ShowMovie($movie3);
 
-    $actors = array ("James Duval","Fairuza Balk", "Joseph Gordon-Levitt", "Anjelica Huston", "Dev Patel");
+    // $actors = array ("James Duval","Fairuza Balk", "Joseph Gordon-Levitt", "Anjelica Huston", "Dev Patel");
+    // function ShowActors (array $array) : void {
+    //     foreach ($array as $value) {
+    //         echo ("- $value\n");
+    //     }
+    // };
+    // ShowActors($actors);
+    // unset($actors[2]);
+    // ShowActors($actors);
     
+    // if (in_array("James Duval", $actors)) {
+    //     echo array_search("James Duval", $actors);
+    // } else {
+    //     echo "not found!";
+    // };
+    // $actorsCopy = array();
+    // foreach ($actors as $value) {
+    //     array_push($actorsCopy, $value);
+    // };
+    // print_r($actorsCopy);
 
+    $wad21 = array("Anais", "Assmaa", "Caroline", "Grace", "Judy", "Julie", "Julie", "Laure", "Marwa", "Minh", "Myriam", "Stefania", "Valerie", "Ylenia");
+    function randomWad (array $array) : string {
+            $random = rand(0, count($array) - 1);
+            $name = $array[$random];
+            if (in_array($name, $wad21once)) {
+                echo ("Try again");
+                break;
+            } else {
+                array_push($wad21once, $name);
+                echo "$name";
+            };
+        return $name;
+    };
+    $wad21once = array();
+    while (count($wad21once) < count($wad21)) {
+        randomWad($wad21);
+    };
 
 ?>
